1e20d09c8e21320790cd6fa1684e1622ce18eb59
John Velis <velisj@ad.nmc.edu>
2019-03-20 13:49:13 -0400

    Add project files.

---
+++ ArrayOfFruit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfFruit
+{
+    class Program
+    {
+        public enum Fruit
+        {
+            none,
+            banana,
+            strawberry,
+            apple,
+            orange
+        }
+
+        static void Main(string[] args)
+        {
+            Fruit[] fruits = new Fruit[3];
+            string userResponse;
+
+            for (int index = 0; index < 3; index++)
+            {
+                Console.Write($"Enter Fruit #{index + 1}:");
+                userResponse = Console.ReadLine();
+                Enum.TryParse(userResponse,out fruits[index]);
+            }
+
+            Console.WriteLine();
+
+            foreach (Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
